041124b189eeb82e3315ad650d1372d8eb110844
Victor Muñoz Palomino <victor.munoz@jobandtalent.com>
2016-12-06 17:52:16 +0100

    Solve 15

--- src/ii_collections/_15_AllAnyAndOtherPredicates.kt
+++ src/ii_collections/_15_AllAnyAndOtherPredicates.kt
@@ -15,25 +15,25 @@ fun example2(list: List<Int>) {
 
 fun Customer.isFrom(city: City): Boolean {
     // Return true if the customer is from the given city
-    return this.city.equals(city)
+    return city.equals(this.city)
 }
 
 fun Shop.checkAllCustomersAreFrom(city: City): Boolean {
     // Return true if all customers are from the given city
-    todoCollectionTask()
+    return this.countCustomersFrom(city) == this.customers.count()
 }
 
 fun Shop.hasCustomerFrom(city: City): Boolean {
     // Return true if there is at least one customer from the given city
-    todoCollectionTask()
+    return findAnyCustomerFrom(city) != null
 }
 
 fun Shop.countCustomersFrom(city: City): Int {
     // Return the number of customers from the given city
-    todoCollectionTask()
+    return customers.filter { it.isFrom(city) }.count()
 }
 
 fun Shop.findAnyCustomerFrom(city: City): Customer? {
     // Return a customer who lives in the given city, or null if there is none
-    todoCollectionTask()
+    return this.customers.firstOrNull { it.isFrom(city) }
 }
