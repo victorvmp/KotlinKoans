a8bce024e7dd27bf49af8de42831b4e230ec8900
Victor Muñoz Palomino <victor.munoz@jobandtalent.com>
2016-12-06 11:23:03 +0100

    Solve 1

--- src/i_introduction/_1_Java_To_Kotlin_Converter/JavaToKotlinConverter.kt
+++ src/i_introduction/_1_Java_To_Kotlin_Converter/JavaToKotlinConverter.kt
@@ -13,5 +13,5 @@ fun todoTask1(collection: Collection<Int>): Nothing = TODO(
 
 
 fun task1(collection: Collection<Int>): String {
-    todoTask1(collection)
+    return JavaCode1().task1(collection)
 }
