87956d912ecdf66100532dd189c29269e42dcad3
Victor Muñoz Palomino <victor.munoz@jobandtalent.com>
2016-12-06 21:21:05 +0100

    Solve 26

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,19 +1,21 @@
 package iii_conventions
 
-import java.util.*
+import java.util.Calendar.*
 
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
     override fun compareTo(other: MyDate): Int {
-        return toMillis(this).compareTo(toMillis(other))
+        return toMillis().compareTo(other.toMillis())
     }
 
     fun MyDate.compareTo(other: MyDate): Boolean {
         return this.compareTo(other) == 0
     }
 
-    fun toMillis(date: MyDate): Long {
-        val instance = Calendar.getInstance()
-        instance.set(date.year, date.month, date.dayOfMonth)
+    fun toMillis(): Long {
+        val instance = getInstance()
+        instance.set(YEAR, year)
+        instance.set(MONTH, month - 1)
+        instance.set(DAY_OF_MONTH, dayOfMonth)
         return instance.timeInMillis
     }
 }
@@ -27,4 +29,10 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(override val start: MyDate, override val endInclusive: MyDate) : ClosedRange<MyDate> {
+    override fun contains(value: MyDate): Boolean {
+        val origin = start.toMillis()
+        val other = endInclusive.toMillis()
+        return value.toMillis() >= origin && value.toMillis() <= other
+    }
+}
--- src/iii_conventions/_26_InRange.kt
+++ src/iii_conventions/_26_InRange.kt
@@ -4,7 +4,7 @@ import util.TODO
 import util.doc26
 
 fun todoTask26_(): Nothing = TODO(
-    """
+        """
         Task 26.
         Uncomment the commented line.
         In Kotlin 'in' checks are translated to the corresponding 'contains' calls.
@@ -12,11 +12,10 @@ fun todoTask26_(): Nothing = TODO(
         Alternatively, you can make the class DateRange implement the standard ClosedRange interface.
         Add all changes to the file MyDate.kt.
     """,
-    documentation = doc26(),
-    references = { range: ClosedRange<Int> -> }
+        documentation = doc26(),
+        references = { range: ClosedRange<Int> -> }
 )
 
 fun checkInRange(date: MyDate, first: MyDate, last: MyDate): Boolean {
-    todoTask26_()
-//    return date in DateRange(first, last)
+    return date in DateRange(first, last)
 }
