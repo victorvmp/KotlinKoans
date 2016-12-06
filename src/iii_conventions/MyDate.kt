a05369ecf4ae5620a36f3e852ffd5fc5b2b232f3
Victor Muñoz Palomino <victor.munoz@jobandtalent.com>
2016-12-06 20:51:28 +0100

    Solve 25

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,23 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+import java.util.*
+
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        return toMillis(this).compareTo(toMillis(other))
+    }
+
+    fun MyDate.compareTo(other: MyDate): Boolean {
+        return this.compareTo(other) == 0
+    }
+
+    fun toMillis(date: MyDate): Long {
+        val instance = Calendar.getInstance()
+        instance.set(date.year, date.month, date.dayOfMonth)
+        return instance.timeInMillis
+    }
+}
+
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
--- src/iii_conventions/_25_Comparison_.kt
+++ src/iii_conventions/_25_Comparison_.kt
@@ -4,18 +4,17 @@ import util.TODO
 import util.doc25
 
 fun todoTask25(): Nothing = TODO(
-    """
+        """
         Task 25.
         Uncomment the commented line and make it compile. 
         Add all changes to the file MyDate.kt.
         Make class MyDate implement Comparable.
     """,
-    documentation = doc25(),
-    references = { date: MyDate, comparable: Comparable<MyDate> -> }
+        documentation = doc25(),
+        references = { date: MyDate, comparable: Comparable<MyDate> -> }
 )
 
 fun task25(date1: MyDate, date2: MyDate): Boolean {
-    todoTask25()
-//    return date1 < date2
+    return date1 < date2
 }
 
