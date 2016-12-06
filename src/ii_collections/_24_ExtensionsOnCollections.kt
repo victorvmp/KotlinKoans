ff55bf14bedee5257a920eba17248b5a350029ec
Victor Muñoz Palomino <victor.munoz@jobandtalent.com>
2016-12-06 20:20:44 +0100

    Solve 24

--- src/ii_collections/_24_ExtensionsOnCollections.kt
+++ src/ii_collections/_24_ExtensionsOnCollections.kt
@@ -3,7 +3,7 @@ package ii_collections
 import util.TODO
 
 fun todoTask24(): Nothing = TODO(
-    """
+        """
         Task 24.
         The function should behave the same as '_24_JavaCode.doSomethingStrangeWithCollection'
         Replace all invocations of 'todoTask24()' with the appropriate code.
@@ -12,8 +12,8 @@ fun todoTask24(): Nothing = TODO(
 )
 
 fun doSomethingStrangeWithCollection(collection: Collection<String>): Collection<String>? {
-    val groupsByLength = collection.groupBy { s -> todoTask24() }
+    val groupsByLength = collection.groupBy(String::length)
 
-    return groupsByLength.values.maxBy { group -> todoTask24() }
+    return groupsByLength.values.maxBy { group -> group.size > 0 }
 }
 
