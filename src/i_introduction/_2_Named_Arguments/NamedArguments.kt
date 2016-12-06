c9c9c1012b0ca213be995c08e485f2c9c69f85b5
Victor Muñoz Palomino <victor.munoz@jobandtalent.com>
2016-12-06 11:30:26 +0100

    Solve 2

--- src/i_introduction/_2_Named_Arguments/NamedArguments.kt
+++ src/i_introduction/_2_Named_Arguments/NamedArguments.kt
@@ -5,7 +5,8 @@ import util.TODO
 import util.doc2
 
 // default values for arguments:
-fun bar(i: Int, s: String = "", b: Boolean = true) {}
+fun bar(i: Int, s: String = "", b: Boolean = true) {
+}
 
 fun usage() {
     // named arguments:
@@ -13,15 +14,14 @@ fun usage() {
 }
 
 fun todoTask2(): Nothing = TODO(
-    """
+        """
         Task 2.
         Implement the same logic as in 'task1' again through the library method 'joinToString()'.
         Specify only two of the 'joinToString' arguments.
     """,
-    documentation = doc2(),
-    references = { collection: Collection<Int> -> task1(collection); collection.joinToString() })
+        documentation = doc2(),
+        references = { collection: Collection<Int> -> task1(collection); collection.joinToString() })
 
 fun task2(collection: Collection<Int>): String {
-    todoTask2()
-    return collection.joinToString()
+    return collection.joinToString(prefix = "{", postfix = "}")
 }
